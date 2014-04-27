b7fd88c72aaa6dc1ff5f3f8108f5f17749648f6e
AJ Fite <goldman60@nclf.net>
2014-04-27 15:31:30 -0700

    Added google analytics code
    I'm interested in seeing what the usage is like

--- index.php
+++ index.php
@@ -79,5 +79,16 @@ $endyear = $startyear + 21;
             <p>Ghetto Fabulous V1.2 - Now with more correctness and less eye bleeding!</p>
             <p>&copy; 2014 <a href="http://ajfite.com">A.J. Fite</a> -- <a href="https://github.com/Goldman60/RotationCalculator">Open Source on GitHub</a> -- <a href="LICENSE.html">License</a></p>
         </footer>
+        
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50434529-1', 'ajfite.com');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
